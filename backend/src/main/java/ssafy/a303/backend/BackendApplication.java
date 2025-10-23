50adb1d59c46598c03b7186be1a295730f0b69ed
Joonho <junho9661@gmail.com>
2025-10-23 16:57:03 +0900

    setting: 백엔드 project 생성 [S13P31A303-2] - application.yml, application-dev.yml, application-prod.yml로 분리 - postgresql

---
+++ backend/src/main/java/ssafy/a303/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package ssafy.a303.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
