cc2d24a02dd21ce3e01511904dc211dd2ef01488
김도현 <dojin8351@gmail.com>
2025-10-28 09:31:52 +0900

    feat : KakaoLoader Next.js 버전에 더 좋은 방법으로 수정

--- frontend/src/app/(tabs-bottom)/map/page.tsx
+++ frontend/src/app/(tabs-bottom)/map/page.tsx
@@ -1,11 +1,35 @@
 'use client'
 
-import KakaoMapLoader from '@/components/layout/KakaoMapLoader'
+import { Map } from 'react-kakao-maps-sdk'
 
-export default function MapPage() {
+import useKakaoLoader from '@/hook/useKakaoLoader'
+
+export default function BasicMap() {
+  useKakaoLoader()
+
   return (
-    <section className="fixed inset-0 top-0 bottom-[73px] z-0 h-screen w-full">
-      <KakaoMapLoader />
-    </section>
+    <div className="relative h-screen w-full">
+      {/* 지도 */}
+      <Map
+        id="map"
+        center={{ lat: 33.450701, lng: 126.570667 }}
+        style={{ width: '100%', height: '100%' }}
+        level={3}
+        className="absolute inset-0 z-0"
+      />
+
+      <div className="pointer-events-none absolute inset-0">
+        <button
+          onClick={() => console.log('안녕 클릭')}
+          className="pointer-events-auto absolute top-4 left-4 z-10 rounded-lg bg-white px-4 py-2 shadow"
+        >
+          안녕 👋
+        </button>
+
+        <button className="pointer-events-auto absolute right-6 bottom-6 z-10 rounded-full bg-blue-500 px-4 py-2 text-white shadow-lg">
+          내 위치로 이동
+        </button>
+      </div>
+    </div>
   )
 }
---
+++ frontend/src/hook/useKakaoLoader.ts
@@ -0,0 +1,9 @@
+// 📁 hooks/useKakaoLoader.ts
+import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk'
+
+export default function useKakaoLoader() {
+  useKakaoLoaderOrigin({
+    appkey: process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY as string,
+    libraries: ['clusterer', 'drawing', 'services'],
+  })
+}
