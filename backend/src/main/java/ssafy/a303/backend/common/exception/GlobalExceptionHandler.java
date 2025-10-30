4d09f5df188868bf5388bc8ac003eebe7972a1a8
주연우 <juywoo@naver.com>
2025-10-30 14:01:05 +0900

    setting : be 에러 응답 형식 통일 [S13P31A303-118]

--- backend/src/main/java/ssafy/a303/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/ssafy/a303/backend/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ssafy.a303.backend.common.dto.CustomException;
@@ -14,15 +15,15 @@ import ssafy.a303.backend.common.response.ErrorCode;
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(CustomException.class)
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<ResponseDTO<?>> handleCustomException(CustomException e) {
         return ResponseEntity.status(e.getHttpStatus())
                 .body(ResponseDTO.fail(e.getErrorCode()));
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
+    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
     public ResponseEntity<ResponseDTO<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
