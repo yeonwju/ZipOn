b30aea51cb7b1ee2e3bffdc41b5596b1ee8e0a11
김도현 <dojin8351@gmail.com>
2025-11-03 16:29:11 +0900

    [S13P31A303-156] style : 스타일링 수정

--- frontend/src/components/listing/ListingInfo.tsx
+++ frontend/src/components/listing/ListingInfo.tsx
@@ -1,6 +1,6 @@
 import { CalendarDays, Grid3x3, House, Layers, MapPin } from 'lucide-react'
 
-import ListingInfoItem from '@/components/listing/ui/ListingInfoItem'
+import ListingInfoItem from '@/components/listing/ListingInfoItem'
 
 interface ListingInfoProps {
   name: string
