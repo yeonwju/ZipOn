f1d2d7487b2d84a74361d1a3275718f4ba35b4de
Joonho <junho9661@gmail.com>
2025-11-03 17:02:22 +0900

    feat : 유저 정보 [S13P31A303-136] - 본인 조회 기능 - Broker, Company 엔티티 생성 - 사업자 현재 운영 중 확인 기능 추가 - 사업자 운영 확인 시 쿠키 발급 - 사업자 운영 확인 시 bizno에서 회사명 검색 후 DB 저장 - BizNo API 하루 한도 초과시 오류 예외 처리 - 사업자등록번호 저장시 "-" 빼지 않던 오류 해결

--- backend/src/main/java/ssafy/a303/backend/broker/controller/CompanyController.java
+++ backend/src/main/java/ssafy/a303/backend/broker/controller/CompanyController.java
@@ -1,9 +1,7 @@
 package ssafy.a303.backend.broker.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
