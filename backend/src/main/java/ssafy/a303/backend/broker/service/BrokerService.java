c12c2c1d9295bf705bc4a9a6254631f787ffc92d
Joonho <junho9661@gmail.com>
2025-11-04 16:35:16 +0900

    feat : 유저 정보 [S13P31A303-136] - 본인 조회 기능 - Broker, Company 엔티티 생성 - 사업자 현재 운영 중 확인 기능 추가 - 사업자 운영 확인 시 쿠키 발급 - 사업자 운영 확인 시 bizno에서 회사명 검색 후 DB 저장 - BizNo API 하루 한도 초과시 오류 예외 처리 - 사업자등록번호 저장시 "-" 빼지 않던 오류 해결 - application-dev.yml postgres url 설정 적절하게 변수 대입, 시간 설정 - ssafy api 오류 시 exception 발생하여 뒤에 코드 발동 안하는 버그 수정

--- backend/src/main/java/ssafy/a303/backend/broker/service/BrokerService.java
+++ backend/src/main/java/ssafy/a303/backend/broker/service/BrokerService.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 import ssafy.a303.backend.broker.entity.Broker;
 import ssafy.a303.backend.broker.entity.Company;
 import ssafy.a303.backend.broker.repository.BrokerRepository;
-import ssafy.a303.backend.broker.repository.CompanyRepository;
 import ssafy.a303.backend.common.exception.CustomException;
 import ssafy.a303.backend.common.response.ErrorCode;
 import ssafy.a303.backend.user.entity.User;
 import ssafy.a303.backend.user.repository.UserRepository;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,12 +22,12 @@ public class BrokerService {
     @Transactional
     public boolean regist(int userSeq, String taxSeq) {
         Company company = companyService.checkCompany(taxSeq); // 사업체 확인 또는 등록
-        if(!company.getStatus()) throw new CustomException(ErrorCode.INVALID_TAX_SEQ);
+        if (!company.getStatus()) throw new CustomException(ErrorCode.INVALID_TAX_SEQ);
         User userRef = userRepository.getReferenceById(userSeq);
 
         // S3 key 발급 받으면 저장할 예정
         String s3Path = String.format("license/%d", userSeq);
-        if(!saveLicensePdf()) throw new CustomException(ErrorCode.EXTERNAL_API_ERROR);
+        if (!saveLicensePdf()) throw new CustomException(ErrorCode.EXTERNAL_API_ERROR);
 
         Broker broker = Broker.builder()
                 .company(company)
@@ -43,7 +40,7 @@ public class BrokerService {
         return true;
     }
 
-    public boolean saveLicensePdf(){
+    public boolean saveLicensePdf() {
         return true;
     }
 
