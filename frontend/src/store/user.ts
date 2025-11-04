7de5fe7efb61986a5a8651905298d1545f762d63
김도현 <dojin8351@gmail.com>
2025-11-04 19:48:12 +0900

    [S13P31A303-162] feat : zustand authStore 생성

---
+++ frontend/src/store/user.ts
@@ -0,0 +1,44 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export interface User {
+  name: string
+  nickname: string | null
+  tel: string
+  birth: string
+  profileImg: string
+  socialType: string | null
+  isBroker: boolean
+  isVerified: boolean
+}
+
+interface UserState {
+  user: User | null
+  setUser: (user: Partial<User>) => void
+  clearUser: () => void
+}
+
+export const useUserStore = create<UserState>()(
+  persist(
+    set => ({
+      user: {
+        name: '김도현',
+        nickname: '김도현 닉네임',
+        tel: '010-1234-5678',
+        birth: '1990-01-01',
+        profileImg: '/profile.svg',
+        socialType: 'google',
+        isBroker: true,
+        isVerified: true,
+      },
+
+      setUser: user =>
+        set(state => ({
+          user: { ...state.user, ...user } as User,
+        })),
+
+      clearUser: () => set({ user: null }),
+    }),
+    { name: 'user-storage' }
+  )
+)
