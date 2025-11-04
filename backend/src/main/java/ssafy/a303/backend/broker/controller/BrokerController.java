d9c5e2fc775f4011a5d0280ba386f7a4e6b2e570
Joonho <junho9661@gmail.com>
2025-11-04 16:34:19 +0900

    feat : 유저 정보 [S13P31A303-136] - 본인 조회 기능 - Broker, Company 엔티티 생성 - 사업자 현재 운영 중 확인 기능 추가 - 사업자 운영 확인 시 쿠키 발급 - 사업자 운영 확인 시 bizno에서 회사명 검색 후 DB 저장 - BizNo API 하루 한도 초과시 오류 예외 처리 - 사업자등록번호 저장시 "-" 빼지 않던 오류 해결 - application-dev.yml postgres url 설정 적절하게 변수 대입, 시간 설정

--- backend/src/main/java/ssafy/a303/backend/broker/controller/BrokerController.java
+++ backend/src/main/java/ssafy/a303/backend/broker/controller/BrokerController.java
@@ -2,11 +2,13 @@ package ssafy.a303.backend.broker.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ssafy.a303.backend.broker.dto.request.BrokerInfoRequest;
+import ssafy.a303.backend.broker.service.BrokerService;
 import ssafy.a303.backend.common.response.ResponseDTO;
 
 @RestController
@@ -14,9 +16,11 @@ import ssafy.a303.backend.common.response.ResponseDTO;
 @RequestMapping("/api/v1/broker")
 public class BrokerController {
 
-    @PostMapping
-    public ResponseEntity<ResponseDTO<Void>> enroll(@RequestBody BrokerInfoRequest brokerInfoRequest) {
+    private final BrokerService brokerService;
 
+    @PostMapping
+    public ResponseEntity<ResponseDTO<Void>> enroll(@AuthenticationPrincipal int userSeq, @RequestBody BrokerInfoRequest brokerInfoRequest) {
+        brokerService.regist(userSeq, brokerInfoRequest.taxSeq());
         return ResponseDTO.ok(null, "작업중");
     }
 }
