25e7633b58493fd7847c8616576235916b9b940a
Joonho <junho9661@gmail.com>
2025-11-04 19:52:04 +0900

    feat : 유저 정보 [S13P31A303-136] - 본인 조회 기능 - Broker, Company 엔티티 생성 - 사업자 현재 운영 중 확인 기능 추가 - 사업자 운영 확인 시 쿠키 발급 - 사업자 운영 확인 시 bizno에서 회사명 검색 후 DB 저장 - BizNo API 하루 한도 초과시 오류 예외 처리 - 사업자등록번호 저장시 "-" 빼지 않던 오류 해결 - application-dev.yml postgres url 설정 적절하게 변수 대입, 시간 설정 - ssafy api 오류 시 exception 발생하여 뒤에 코드 발동 안하는 버그 수정 - user 처음 저장시 name 빼고 nickname만 저장하겠금 변경 - 로그인 성공 시 도착지점 무조건 fronturl/auth/success으로 변경

--- backend/src/main/java/ssafy/a303/backend/broker/controller/BrokerController.java
+++ backend/src/main/java/ssafy/a303/backend/broker/controller/BrokerController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import ssafy.a303.backend.broker.dto.request.BrokerInfoRequest;
 import ssafy.a303.backend.broker.service.BrokerService;
 import ssafy.a303.backend.common.response.ResponseDTO;
 
