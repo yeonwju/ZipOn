99d1f3934416c58ec877661942b77b4915139215
Joonho <junho9661@gmail.com>
2025-11-05 16:41:20 +0900

    feat : 유저 정보 [S13P31A303-136] - 본인 조회 기능 - Broker, Company 엔티티 생성 - 사업자 현재 운영 중 확인 기능 추가 - 사업자 운영 확인 시 쿠키 발급 - 사업자 운영 확인 시 bizno에서 회사명 검색 후 DB 저장 - BizNo API 하루 한도 초과시 오류 예외 처리 - 사업자등록번호 저장시 "-" 빼지 않던 오류 해결 - application-dev.yml postgres url 설정 적절하게 변수 대입, 시간 설정 - ssafy api 오류 시 exception 발생하여 뒤에 코드 발동 안하는 버그 수정 - user 처음 저장시 name 빼고 nickname만 저장하겠금 변경 - 로그인 성공 시 도착지점 무조건 fronturl/auth/success으로 변경 - sms servcie 하는 중

--- backend/src/main/java/ssafy/a303/backend/sms/service/SmsService.java
+++ backend/src/main/java/ssafy/a303/backend/sms/service/SmsService.java
@@ -1,16 +1,52 @@
 package ssafy.a303.backend.sms.service;
 
+import com.solapi.sdk.SolapiClient;
+import com.solapi.sdk.message.model.Message;
+import com.solapi.sdk.message.dto.response.MultipleDetailMessageSentResponse;
+import com.solapi.sdk.message.service.DefaultMessageService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SmsService {
+
     @Value("${solapi.key}")
-    private String solapiKey;
+    String solapiKey;
     @Value("${solapi.secret-key}")
-    private String solapiSecretKey;
+    String solapiSecretKey;
+    @Value("${solapi.tel}")
+    String solapiTel;
+    DefaultMessageService messageService;
 
-    public void send(String code){
+    private static String randomCode() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            int rand = (int) (Math.random() * 26) + 65;
+            sb.append((char) rand);
+        }
+        return sb.toString();
+    }
 
+    @PostConstruct
+    void init() {
+        this.messageService = SolapiClient.INSTANCE.createInstance(solapiKey, solapiSecretKey);
+    }
+
+    public MultipleDetailMessageSentResponse send(String tel) {
+        try {
+            Message message = new Message();
+            message.setFrom(solapiTel);
+            message.setTo(tel);
+            message.setText(String.format("[집온] 인증 번호입니다. %s", randomCode()));
+
+            MultipleDetailMessageSentResponse response = this.messageService.send(message);
+            System.out.println(response);
+
+            return response;
+        } catch (Exception e) {
+            e.fillInStackTrace();
+        }
+        return null;
     }
 }
