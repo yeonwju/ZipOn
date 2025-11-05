df3d3b01b3710bd5ec9b0d247a229e9dee7fcb1b
Joonho <junho9661@gmail.com>
2025-11-05 09:47:29 +0900

    feat : 유저 정보 [S13P31A303-136] - 본인 조회 기능 - Broker, Company 엔티티 생성 - 사업자 현재 운영 중 확인 기능 추가 - 사업자 운영 확인 시 쿠키 발급 - 사업자 운영 확인 시 bizno에서 회사명 검색 후 DB 저장 - BizNo API 하루 한도 초과시 오류 예외 처리 - 사업자등록번호 저장시 "-" 빼지 않던 오류 해결 - application-dev.yml postgres url 설정 적절하게 변수 대입, 시간 설정 - ssafy api 오류 시 exception 발생하여 뒤에 코드 발동 안하는 버그 수정 - user 처음 저장시 name 빼고 nickname만 저장하겠금 변경 - 로그인 성공 시 도착지점 무조건 fronturl/auth/success으로 변경

--- backend/src/main/java/ssafy/a303/backend/broker/controller/BrokerController.java
+++ backend/src/main/java/ssafy/a303/backend/broker/controller/BrokerController.java
@@ -20,7 +20,7 @@ public class BrokerController {
     private final BrokerService brokerService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ResponseDTO<Void>> enrollSimple(
+    public ResponseEntity<ResponseDTO<Void>> enroll(
             @AuthenticationPrincipal int userSeq,
             @RequestPart("taxSeq") String taxSeq,
             @RequestPart("license") MultipartFile licensePdf
