cda8f6339f8edee050d4540d90d20341321e9ac4
Joonho <junho9661@gmail.com>
2025-11-07 13:28:54 +0900

    feat: auction-entity[S13P31A303-189] - 엔티티 생성 (Attendance, Auction) - 엔티티 상태 분기처리에 필요한 enum 타입 생성 (AttendanceStatus, AuctionStatus, Canceler)

---
+++ backend/src/main/java/ssafy/a303/backend/auction/entity/Attendance.java
@@ -0,0 +1,38 @@
+package ssafy.a303.backend.auction.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ssafy.a303.backend.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "attendance")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int attendanceSeq;
+    @ManyToOne
+    @JoinColumn(name = "user_seq", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "auction_seq", nullable = false)
+    private Auction auction;
+    @Column
+    private LocalDateTime bidAt;
+    @Column
+    private int bidAmount;
+    @Column
+    private int rank;
+    @Enumerated(EnumType.STRING)
+    @Column
+    @Builder.Default
+    private AttendanceStatus status = AttendanceStatus.WAITING;
+    @Column
+    private LocalDateTime decidedAt;
+}
