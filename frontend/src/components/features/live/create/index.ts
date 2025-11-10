4c71c345ec54e25c51a3a5873a7a7957b4aff209
김도현 <dojin8351@gmail.com>
2025-11-10 20:03:55 +0900

    [S13P31A303-203] fix : Barrel Export 최신화

---
+++ frontend/src/components/features/live/create/index.ts
@@ -0,0 +1,10 @@
+/**
+ * 라이브 생성 컴포넌트 Barrel Export
+ */
+
+export { default as LiveCreateButton } from './LiveCreateButton'
+export { default as LiveCreateContent } from './LiveCreateContent'
+export { default as LiveMapPreview } from './LiveMapPreview'
+export { default as LiveTitleInput } from './LiveTitleInput'
+export { default as SelectPicker } from './SelectPicker'
+
--- frontend/src/components/features/live/index.ts
+++ frontend/src/components/features/live/index.ts
@@ -1,11 +1,17 @@
 /**
  * 라이브 컴포넌트 Barrel Export
+ * 
+ * 하위 폴더별로 정리:
+ * - create: 라이브 생성 관련
+ * - list: 라이브 목록 관련
+ * - on-air: 실시간 방송 관련
  */
 
-export { default as LiveCreateButton } from './LiveCreateButton'
-export { default as LiveItem } from './LiveItem'
-export { default as LiveItems } from './LiveItems'
-export { default as LiveMapPreview } from './LiveMapPreview'
-export { default as LiveTitleInput } from './LiveTitleInput'
-export { default as MiniPlayer } from './MiniPlayer'
-export { default as SelectPicker } from './SelectPicker'
+// 라이브 생성
+export * from './create'
+
+// 라이브 목록
+export * from './list'
+
+// 라이브 방송 (on-air)
+export * from './on-air'
