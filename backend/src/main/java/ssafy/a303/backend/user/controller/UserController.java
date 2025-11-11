062e13569e88866ed25662ac4b1d9affc914a155
Joonho <junho9661@gmail.com>
2025-11-11 10:06:50 +0900

    hotfix: yml forward-headers-strategy: framework로 수정

--- backend/src/main/java/ssafy/a303/backend/user/controller/UserController.java
+++ backend/src/main/java/ssafy/a303/backend/user/controller/UserController.java
@@ -1,5 +1,9 @@
 package ssafy.a303.backend.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,11 +18,21 @@ import ssafy.a303.backend.user.service.UserService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
+@Tag(name = "User", description = "사용자 정보 관련 API")
 public class UserController {
 
     private final UserService userService;
     private final SmsService smsService;
 
+    @Operation(
+            summary = "내 정보 조회",
+            description = "현재 로그인한 사용자의 상세 정보를 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 실패"),
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
+    })
     @GetMapping("/me")
     public ResponseEntity<ResponseDTO<MeResponseDTO>> getUser(@AuthenticationPrincipal Integer userSeq) {
         MeResponseDTO meResponseDTO = userService.getUser(userSeq);
