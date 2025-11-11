394045be08061a8805d26700e67bbb052f1c1db3
주연우 <juywoo@naver.com>
2025-11-11 09:30:28 +0900

    chore : 이미지 dto swagger 설정

--- backend/src/main/java/ssafy/a303/backend/property/dto/response/ImageDto.java
+++ backend/src/main/java/ssafy/a303/backend/property/dto/response/ImageDto.java
@@ -1,10 +1,14 @@
 package ssafy.a303.backend.property.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 public record ImageDto(
+        @Schema(description = "이미지 s3 키", example = "lkjasfoejl")
         String s3key,
+        @Schema(description = "s3 주소", example = "www.dfjkslfs.sdjflsafjk")
         String url,
+        @Schema(description = "사진 순서", example = "1")
         Integer order
 ) {
 }
