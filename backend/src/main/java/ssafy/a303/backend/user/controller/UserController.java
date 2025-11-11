c1d0fbb4de25a7443a2de9c447a1e5d2ea55e73c
Joonho <junho9661@gmail.com>
2025-11-11 15:33:01 +0900

    feat: broker, company swagger [S13P31303-204]

--- backend/src/main/java/ssafy/a303/backend/user/controller/UserController.java
+++ backend/src/main/java/ssafy/a303/backend/user/controller/UserController.java
@@ -2,6 +2,7 @@ package ssafy.a303.backend.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -30,7 +31,7 @@ public class UserController {
             description = "현재 로그인한 사용자의 상세 정보를 조회합니다."
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MeResponseDTO.class))),
             @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다.", content = @Content())
     })
     @GetMapping("/me")
@@ -59,7 +60,7 @@ public class UserController {
             description = "발송된 인증번호를 검증하고, 성공 시 사용자 정보를 갱신하여 반환합니다."
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "인증 성공 및 사용자 정보 반환", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "200", description = "인증 성공 및 사용자 정보 반환", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MeResponseDTO.class))),
             @ApiResponse(responseCode = "400", description = "잘못된 인증번호", content = @Content()),
             @ApiResponse(responseCode = "401", description = "인증 실패: 토큰이 없거나 유효하지 않음", content = @Content()),
             @ApiResponse(responseCode = "404", description = "인증 대상 정보 없음: 코드 미발송(SMS_NOT_SENDED) 또는 사용자 없음(USER_NOT_FOUND)", content = @Content())
