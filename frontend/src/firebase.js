58425e88a3354a59244fed276f6ac17bff303b41
bkw0820 <bkw0820@naver.com>
2025-11-12 10:19:16 +0900

    feat: firebaseConfig 추가 [S13P31A303-206]

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDpO3fOehMwqRvbujS9MKHZs1Tb7ht7XVI',
+  authDomain: 'zipon-b45c7.firebaseapp.com',
+  projectId: 'zipon-b45c7',
+  storageBucket: 'zipon-b45c7.firebasestorage.app',
+  messagingSenderId: '270803338199',
+  appId: '1:270803338199:web:40ce961744ab98211a0192',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
