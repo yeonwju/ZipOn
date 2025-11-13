972d19694e99baae1c90c411c0d17ada7a17b25e
Joonho <junho9661@gmail.com>
2025-11-13 17:52:21 +0900

    feat: kafka [ㄴ13ㅔ31ㅁ303-216] - 설정 중

---
+++ backend/src/main/java/ssafy/a303/backend/auction/dto/request/BidEventMessage.java
@@ -0,0 +1,17 @@
+package ssafy.a303.backend.auction.dto.request;
+
+import ssafy.a303.backend.common.helper.KoreaClock;
+
+import java.time.LocalDateTime;
+
+public record BidEventMessage(
+        Integer auctionSeq,
+        Integer userSeq,
+        Long amount,
+        LocalDateTime bidAt
+) {
+    public static BidEventMessage of(Integer auctionSeq, Integer userSeq, Long amount){
+        LocalDateTime now = KoreaClock.nowLocalDateTime();
+        return new BidEventMessage(auctionSeq, userSeq, amount, now);
+    }
+}
--- backend/src/main/java/ssafy/a303/backend/common/helper/KoreaClock.java
+++ backend/src/main/java/ssafy/a303/backend/common/helper/KoreaClock.java
@@ -3,6 +3,7 @@ package ssafy.a303.backend.common.helper;
 import java.sql.Timestamp;
 import java.time.Clock;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class KoreaClock {
@@ -10,8 +11,12 @@ public class KoreaClock {
         return Clock.system(ZoneId.of("Asia/Seoul"));
     }
 
-    public static Timestamp now() {
+    public static Timestamp nowTimeStamp() {
         return Timestamp.from(kClock().instant());
+    }
+
+    public static LocalDateTime nowLocalDateTime() {
+        return LocalDateTime.from(kClock().instant());
     }
 
     public static LocalDate today() {
