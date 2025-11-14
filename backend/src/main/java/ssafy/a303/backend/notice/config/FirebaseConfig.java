f9b8600f921f1e944a23686c85afb987b3d07f80
bkw0820 <bkw0820@naver.com>
2025-11-14 17:02:42 +0900

    hotfix: firebase key 서버 저장

--- backend/src/main/java/ssafy/a303/backend/notice/config/FirebaseConfig.java
+++ backend/src/main/java/ssafy/a303/backend/notice/config/FirebaseConfig.java
@@ -3,8 +3,9 @@ package ssafy.a303.backend.notice.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
@@ -12,16 +13,18 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
-    @PostConstruct
+    @EventListener(ApplicationReadyEvent.class)
     public void init() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
             GoogleCredentials credentials = GoogleCredentials
                     .fromStream(new ClassPathResource("firebase/service-account-key.json").getInputStream());
+
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(credentials)
                     .build();
+
             FirebaseApp.initializeApp(options);
-            System.out.println("Firebase Admin SDK 초기화 완료");
+            System.out.println("🔥 Firebase Admin SDK 초기화 완료");
         }
     }
 }
